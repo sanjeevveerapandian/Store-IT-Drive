139611047ed642acd5f47a9375bceff34b38bee4
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2025-04-27 03:05:25 +0530

    Auth pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex-center h-screen">
-      <h1 className="h1">StoreIt - The only storage solution you need.</h1>
+      <h1 className="h1">StoreIT - The only storage solution you need.</h1>
     </div>
   );
 }
